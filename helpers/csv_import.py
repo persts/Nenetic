4ac1681b2d75ebd71414ba485ea30979f517b358
pete <ersts@amnh.org>
2018-09-27 12:56:02 -0400

    Removed hard coded size from csv importer

--- helpers/csv_import.py
+++ helpers/csv_import.py
@@ -1,6 +1,7 @@
 import csv
 import sys
 import json
+from PIL import Image
 from random import randint
 
 if len(sys.argv) < 3:
@@ -8,6 +9,8 @@ if len(sys.argv) < 3:
     sys.exit()
 
 FILE = sys.argv[2]
+img = Image.open(FILE)
+height = float(img.size[1])
 package = {'classes': [], 'points': {FILE: {}}, 'colors': {}}
 with open(sys.argv[1]) as csvfile:
     reader = csv.reader(csvfile, delimiter=',')
@@ -18,7 +21,7 @@ with open(sys.argv[1]) as csvfile:
             package['classes'].append(value)
             package['colors'][value] = [randint(0, 255), randint(0, 255), randint(0, 255)]
             package['points'][FILE][value] = []
-        package['points'][FILE][value].append({'x': float(row[0]), 'y': 3000.0 - float(row[1])})
+        package['points'][FILE][value].append({'x': float(row[0]), 'y': height - float(row[1])})
 package['classes'].sort()
 file = open(FILE + '.pnt', 'w')
 json.dump(package, file)
